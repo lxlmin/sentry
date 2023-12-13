45662b00f49247190c26e73a521915991a07d80c
smm <2080814813@qq.com>
2023-12-13 14:34:57 +0800

    公告

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as Sentry from "@sentry/vue";
 
 Vue.config.productionTip = false
+
+Sentry.init({
+  Vue,
+  dsn: "https://21da16202224cf22967383bef5198a29@o4506386918932480.ingest.sentry.io/4506386975490051",
+  integrations: [
+    new Sentry.BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracePropagationTargets: [],
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
 
 new Vue({
   render: h => h(App),
