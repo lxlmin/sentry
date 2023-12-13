184bef9e8273603750f79154d5222b40b6b5fd7f
smm <2080814813@qq.com>
2023-12-13 15:23:15 +0800

    配置监控

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
       //组织名称
       org: 'houpu',
       //项目名称
-      project: "vue-sentry",
+      project: "wyy",
       //密钥
       authToken: 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,
     }),
